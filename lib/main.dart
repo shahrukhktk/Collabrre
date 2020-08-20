695e8df04f7851a97307e74d24c978e29bcf76df
shahrukhktk <Shahrukhktk760@gmail.com>
2020-08-20 08:17:39 -0700

    init progress done

--- lib/main.dart
+++ lib/main.dart
@@ -29,31 +29,77 @@ class _HomeScreenState extends State<HomeScreen> {
                 top: 10,
                 child: Padding(
                   padding: const EdgeInsets.all(15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                  child: Column(
                     children: <Widget>[
-                      Column(
+                      Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
-                          Text(
-                            'Good Morning', style: TextStyle(
-                            color: Colors.grey[700], fontSize: 16
-                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text(
+                                'Good Morning', style: TextStyle(
+                                color: Colors.grey[700], fontSize: 16
+                              ),
+                              ),
+
+                              Text(
+                                'Stephen Hopkins', style: TextStyle(
+                                  color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600, height: 1.5
+                              ),
+                              ),
+                            ],
                           ),
 
-                          Text(
-                            'Stephen Hopkins', style: TextStyle(
-                              color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600, height: 1.5
-                          ),
-                          ),
+                          SizedBox(width: 200,),
+
+                          Icon(Icons.menu),
+
                         ],
                       ),
 
-                      SizedBox(width: 200,),
+                      SizedBox(height: 20,),
 
-                      Icon(Icons.menu),
+                      SizedBox(
+                        width: 360,
+                        height: 150,
+                        child: Card(
+                          elevation: 2,
+                          child: Padding(
+                            padding: const EdgeInsets.all(12.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      'Points You Earned', style: TextStyle(
+                                        color: Colors.black, fontSize: 18, fontWeight: FontWeight.w600, height: 1.5
+                                    ),
+                                    ),
+
+                                    Padding(
+                                      padding: const EdgeInsets.only(top: 15.0),
+                                      child: Text(
+                                        'You have 50 USD Balance', style: TextStyle(
+                                          color: Colors.black, fontSize: 15, fontWeight: FontWeight.normal, height: 1.5
+                                      ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+
+                                Image(image: AssetImage('images/progress.png'),),
+
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
 
                     ],
                   ),
